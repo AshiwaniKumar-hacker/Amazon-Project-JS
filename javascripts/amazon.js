eeb6fcf5dceff8d61126712baa79cb676233b17f
AshiwaniKumar-hacker <kumarashiwani532@gmail.com>
2025-06-22 11:15:59 +0530

    Make the cart quantity interactive

--- javascripts/amazon.js
+++ javascripts/amazon.js
@@ -79,6 +79,14 @@ if(matchingItem){
     });
 }
 
-  console.log(cart)
-})
-})
+let cartQuantity=0;
+cart.forEach((item)=>{
+cartQuantity+=item.quantity;
+});
+
+
+document.querySelector('.js-cart-quantity')
+  .innerHTML=cartQuantity;
+
+});
+});
