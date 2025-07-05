70549c4ccc8e79145368b7846c87df466ec0293b
AshiwaniKumar-hacker <kumarashiwani532@gmail.com>
2025-07-05 23:42:45 +0530

    load products in beforeAll hook

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,11 +1,17 @@
 import { renderOrderSummary } from "../../javascripts/checkout/orderSummary.js";
 import {loadFromStorage} from '../../data/cart.js';
-
+import { loadProducts } from "../../data/products.js";
 
 describe('test suite: renderOrderSummary', () => {
     it('Displays the cart',()=>{
 document.querySelector('.js-test-container').
 innerHTML='<div class="js-order-summary"></div>';
+
+beforeAll((done)=>{
+  loadProducts(()=>{
+   done();
+  });
+});
 
 const productId1='e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
     spyOn(localStorage,'getItem').and.callFake(()=>{
