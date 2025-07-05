2186fe52e9a40ab96e6956b2b6e1a4a28efc357d
AshiwaniKumar-hacker <kumarashiwani532@gmail.com>
2025-07-05 23:34:09 +0530

    load Products from backend

--- javascripts/amazon.js
+++ javascripts/amazon.js
@@ -1,6 +1,10 @@
 import {cart,addToCart } from '../data/cart.js';
-import { products } from '../data/products.js';
+import { products,loadProducts } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
+
+loadProducts(renderProductGrid);
+
+function renderProductGrid(){
 
 let productsHTML='';
 
@@ -82,3 +86,4 @@ button.addEventListener('click',()=>{
   updateCartQuantity();
 });
 });
+}
